d9ab43e5582da915a0dd8c49dee68edf993f493f
王乾润 <2015104036@stu.hqu.edu.cn>
2022-10-04 19:11:01 +0800

    .

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,26 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
+    <router-view />
+    <van-tabbar v-model="active">
+      <van-tabbar-item icon="home-o"></van-tabbar-item>
+      <van-tabbar-item icon="orders-o"></van-tabbar-item>
+      <van-tabbar-item icon="setting-o"></van-tabbar-item>
+    </van-tabbar>
   </div>
 </template>
 
-<style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+<script>
+export default {
+  data() {
+    return {
+      active: 0
     }
   }
+}
+</script>
+
+<style lang="less" scoped>
+.van-tabbar {
+  height: 20vw;
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,18 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import myHome from '@/views/myHome'
 
 Vue.use(VueRouter)
 
 const routes = [
-  {}
+  {
+    path: '/',
+    redirect: 'myHome'
+  },
+  {
+    path: '/myHome',
+    component: myHome
+  }
 ]
 
 const router = new VueRouter({
