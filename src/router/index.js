ed4cda43a8bf7ed0a89bc00e061fefad76d0106c
HHH <2025103026@stu.hqu.edu.cn>
2022-10-04 20:13:58 +0800

    add some folders

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,15 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
+import Home from '../pages/client/Home'
+
 Vue.use(VueRouter)
 
 const routes = [
-  {}
+  {
+    path: '/',
+    component: Home
+  }
 ]
 
 const router = new VueRouter({
