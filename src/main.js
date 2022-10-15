5bc87f0000da72208cd5fbb9477e683bb8e321a6
王乾润 <2015104036@stu.hqu.edu.cn>
2022-10-15 23:41:51 +0800

    自动按需引入

--- src/main.js
+++ src/main.js
@@ -2,13 +2,55 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import Vant, { Lazyload } from 'vant'
-import 'vant/lib/index.css'
 import initAxios from './request'
 import musicApi from './musicApi'
+import {
+  Button,
+  Icon,
+  Image as VanImage,
+  Popup,
+  Lazyload,
+  Calendar,
+  Cascader,
+  Field,
+  Form,
+  Search,
+  DropdownMenu,
+  DropdownItem,
+  Loading,
+  Overlay,
+  Divider,
+  Empty,
+  List,
+  NoticeBar,
+  Sticky,
+  NavBar,
+  Tabbar,
+  TabbarItem
+} from 'vant'
 
-Vue.use(Vant)
+Vue.use(Button)
+Vue.use(Icon)
+Vue.use(VanImage)
+Vue.use(Popup)
 Vue.use(Lazyload)
+Vue.use(Calendar)
+Vue.use(Cascader)
+Vue.use(Field)
+Vue.use(Form)
+Vue.use(DropdownMenu)
+Vue.use(DropdownItem)
+Vue.use(Loading)
+Vue.use(Search)
+Vue.use(Overlay)
+Vue.use(Divider)
+Vue.use(Empty)
+Vue.use(List)
+Vue.use(NoticeBar)
+Vue.use(Sticky)
+Vue.use(NavBar)
+Vue.use(Tabbar)
+Vue.use(TabbarItem)
 
 Vue.config.productionTip = false
 
