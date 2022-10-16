3a2a588725489fbcbb1c6a358c63c62fd04b74b9
王乾润 <2015104036@stu.hqu.edu.cn>
2022-10-16 19:54:49 +0800

    style

--- src/components/SelectMusicList.vue
+++ src/components/SelectMusicList.vue
@@ -24,6 +24,7 @@
       />
     </van-list>
     <van-button
+      round
       type="default"
       @click="confirm"
       v-if="this.musicList.length != 0"
@@ -150,7 +151,7 @@ export default {
     border-top: 1px solid rgb(250, 251, 253);
   }
   .van-button {
-    margin: 2vw 11vw 10vw 11vw;
+    margin: 2vw 5vw 10vw 5vw;
   }
 }
 </style>
