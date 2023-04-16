ba58ea8123f88a3ac992d95c058f6db2415e1c15
王乾润 <2015104036@stu.hqu.edu.cn>
2023-04-16 22:46:18 +0800

    生产环境移除console

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,14 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
+  terser: {
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true
+      }
+    }
+  },
   devServer: {
     proxy: {
       '/user': {
